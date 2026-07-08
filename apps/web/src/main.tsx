70e0dfc3604559d40e30d0b390cef72e426c0b18
nilsduran <nilsdula@gmail.com>
2026-07-08 21:26:19 +0200

    disabled PWA cache to try to fix live API bug fix

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,11 +2,21 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
-import { registerSW } from "virtual:pwa-register";
 import { App } from "./App";
 import "./styles.css";
 
-registerSW({ immediate: true });
+if ("serviceWorker" in navigator) {
+  void navigator.serviceWorker.getRegistrations()
+    .then((registrations) => Promise.all(registrations.map((registration) => registration.unregister())))
+    .then(() => {
+      if ("caches" in window) {
+        return caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key))));
+      }
+      return undefined;
+    })
+    .catch(() => undefined);
+}
+
 const queryClient = new QueryClient({
   defaultOptions: { queries: { retry: 1, staleTime: 30_000 } },
 });
